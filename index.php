68835886b84e4c3e03fe6a73ab471222e3fdf14c
nazmulahsan97 <nazmulahsan97@gmail.com>
2023-07-26 22:06:15 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Bachelors Home</title>
+</head>
+
+<body>
+    <div>
+        <div class="container">
+            <nav>
+                <h1 class="logo">Bachelors Home</h1>
+                <ul class="nav-items">
+                    <div id="inside-nav-items" class="inside-nav-items">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#header-banner">About</a></li>
+                        <li><a href="#popular-collection">Collection</a></li>
+                        <li><a href="#footer-container">Contact</a></li>
+                    </div>
+                </ul>
+                <button class="admin"><a href="admin.php">Admin Login</a></button>
+            </nav>
+
+            <div id="header-banner" class="header-banner">
+                <div class="header-banner-left">
+                    <h1>"Unlock the door to your dream home with us & give your comfort a new name."
+                       </h1>
+                    
+                    <button>
+                        <a id="btn-preebook" href="#popular-collection">Rent Now</a>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+                <div class="header-banner-right">
+                    <img src="./images/house1.jpg" alt="" />
+                </div>
+            </div>
+            <main>
+            <div class="header-banner-left">
+                    <h1>'Also find us on'
+                       </h1>
+
+                <div class="company-brands">
+                    <div class="logo"><img  src="./logos/fb.png" alt="" /></div>
+                    <div class="logo"><img  src="./logos/insta.png" alt="" /></div>
+                    <div class="logo"><img  src="./logos/twitter.png" alt="" /></div>
+                    <div class="logo"><img  src="./logos/whatsapp.png" alt="" /></div>
+                    <div class="logo"><img  src="./logos/youtube.png" alt="" /></div>
+                </div>
+
+                <div id="popular-collection" class="popular-collection">
+                    <h1>Available Rooms</h1>
+                    <div class="popular-collection-items">
+                        <div class="card">
+                            <img src="./images/house2.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">Drawing room available for one person.</h2>
+                                <p class="card-text">BDT: 3300</p>
+                                
+                                <p class="collection-item-description">
+                                location:
+                                  House: 3 , Road: 1 , Block: B , Aftabnogor.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house3.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">One room available </h2>
+                                <p class="card-text">BDT: 5000</p>
+                                
+                                <p class="collection-item-description">
+                                Location: 
+                                  Ansar Camp Bazar, Aftabnogor.
+                                    .
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house4.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">Big master room</h2>
+                                <p class="card-text">BDT: 6000</p>
+                                
+                                <p class="collection-item-description">
+                                Location:
+                                  Bir Uttom Rofikul Islam Avenue/West Merul Badda, Dhaka-1212.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house5.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">White Modern Sport Car</h2>
+                                <p class="card-text">BDT: 7500</p>
+                                  
+                                <p class="collection-item-description">
+                                Location.
+                                    House 33, Road-1 block B, Banasree.
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house6.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">One Seat available</h2>
+                                <p class="card-text">BDT: 3500</p>
+                                
+                                <p class="collection-item-description">
+                                Location : 
+                                  Ansar Camp Bazar,Aftabnagor.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house7.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">Semi masterbed room </h2>
+                                <p class="card-text">BDT: 7000</p>
+                                
+                                <p class="collection-item-description">
+                                Location: 
+                                  Aftabnagar B Block.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house8.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">single room is available</h2>
+                                <p class="card-text">BDT: 2500</p>
+                                
+                                <p class="collection-item-description">
+                                    Location: 
+                                      A block, 3 number road, 37 number House.  Aftabnagar.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house9.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">Single Room</h2>
+                                <p class="card-text">BDT: 5000</p>
+                                
+                                <p class="collection-item-description">
+                                    Location: 
+                                      Aftabnogor,Block A, Road 1.
+                                    
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <img src="./images/house10.jpg" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h2 class="card-title">Single Masrer room</h2>
+                                <p class="card-text">BDT: 4000</p>
+                                
+                                <p class="collection-item-description">
+                                    Location: 
+                                      Aftabnagar e block.
+                                </p>
+                                <button class="button-booknow">Rent</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <footer id="footer-container">
+        <div class="footer-container">
+            <div>
+                <h2>Bachelor Home</h2>
+                <p>Copyright © Team Outliers All rights reserved</p>
+                <div class="footer-icons">
+                    <a href="https://twitter.com/Nazmula61751018" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="https://www.instagram.com/__l_ahsan_l__/" target="_blank"><i
+                            class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/nazmul.ahsan.1535/" target="_blank"><i
+                            class="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.youtube.com/channel/UCfcWFV81PhqdDkZE2oXbAzg" target="_blank"><i
+                            class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+    <!-- Bootstrap -->
+    <script rel="stylesheet" href="node_modules/bootstrap/dist/js/bootstrap.bundle.js"> </script>
+</body>
+
+</html>
